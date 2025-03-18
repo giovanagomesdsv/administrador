8c8d5f63864cbc753c33b64bf73ef169bbe7470d
Giovana Gomes <g.gomesdsv@gmail.com>
2025-03-18 13:55:55 -0300

    home

---
+++ usuarios/usuarios.php
@@ -0,0 +1,72 @@
+<?php
+include "../conexao-banco/conexao.php";
+include "../protecao.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="../geral.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>ADM BC - Usuarios</title>
+</head>
+
+<body>
+<header>
+        Adiministrador BC
+    </header>
+    <nav class="sidebar" id="sidebar"> 
+        <div class="nome">
+            <div class="logo_name">Bem Vindo, <br> <?php echo $_SESSION['nome']; ?>!</div>
+            <div class="menu" id="menu">
+                <i class="bx bx-menu"></i>
+            </div>
+               <div class="linhavaliar">
+                <p>AVALIAR</p>
+               </div>
+        </div>
+        <ul class="nav-list">
+            <li >
+                <a href="../home.php">
+                    <i class='bx bx-home-alt-2'></i>
+                    <span class="link_name">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="../livrarias/livrarias.php">
+                    <i class='bx bx-user'></i>
+                    <span class="link_name">Livrarias</span>
+                </a>
+            </li>
+            <li>
+                <a href="../resenhistas/resenhistas.php">
+                    <i class='bx bx-user-pin'></i>
+                    <span class="link_name">Resenhistas</span>
+                </a>
+            </li>
+            <li >
+                <a href="../livro/livros.php">
+                    <i class='bx bx-book-bookmark'></i>
+                    <span class="link_name">Livros</span>
+                </a>
+            </li>
+            <li class="fix">
+                <a href="usuarios.php">
+                    <i class='bx bx-book-content'></i>
+                    <span class="link_name">Usuarios</span>
+                </a>
+            </li>
+            <li class="sair">
+                <a href="../logout.php"><i class='bx bx-log-out'></i></a>
+            </li>
+        </ul>
+    </nav>
+   
+    <script src="../script.js"></script>
+</body>
+
+</html>
