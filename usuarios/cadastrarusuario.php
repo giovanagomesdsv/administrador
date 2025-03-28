d3b48dc6f4be7ee55b6e6e27b65d6a9d2f37d3f2
CleversonM <cleversonmarceloacelino@gmail.com>
2025-03-28 11:21:25 -0300

    editarusuario.css & cadastrarusuario.css
    adicionado editarusuario.css & cadastrarusuario.css além de finalizado o styles de todas as páginas da parte "usuarios"

--- usuarios/cadastrarusuario.php
+++ usuarios/cadastrarusuario.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <link rel="stylesheet" href="cadastrarusuario.css">
 </head>
-<body>
+<body style="background-color:#DEDEDE">
     <form action="cadastrar.php" method="POST">
 
     <label for="email">E-mail:</label>
